80ca101e0cf2dbb1ffb7f3df959129f543a692d1
Pranjal Nayak <pranjal.nayak@reverie.co.in>
2014-03-04 16:31:57 +0530

    Integrate font rendering engine
    Adapt to 3rd party's font library
    
    (cherry picked from commit 5331a71498c82c1b31e516a3b4e68f8150fb4d34)
    
    Change-Id: Iba7760d7404635e7bd74e68fac226be5478b003a

--- include/freetype/freetype.h
+++ include/freetype/freetype.h
@@ -3992,6 +3992,21 @@ FT_BEGIN_HEADER
   FT_Face_SetUnpatentedHinting( FT_Face  face,
                                 FT_Bool  value );
 
+#ifdef REVERIE
+  FT_EXPORT( FT_UInt16 *)
+  FT_Get_Chars_Indices( FT_Face   face,FT_UInt32  *charcode,int index,
+        FT_UInt32 noofcharcodes,FT_UInt32 *no_of_glyphs);
+
+  FT_EXPORT( FT_Int32)
+  FT_Get_Class( FT_Face   face,FT_UInt16  Gid );
+
+  FT_EXPORT( FT_Int )
+  FT_Get_Position(FT_Face face,FT_UInt16 baseId,FT_UInt16 markId,
+        int *x,int *y,int flag);
+
+#endif
+
+
   /* */
 
 
